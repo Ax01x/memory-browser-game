e7cfb363871a1a6d1916ba195b37cbc6e958ec85
Ax01x <abeeraalk@gmail.com>
2026-07-01 15:02:53 +0300

    created a read me file

---
+++ READ ME.md
@@ -0,0 +1,37 @@
+# Memory Game
+
+_An easy memory card game!_
+
+.. move the memory game and add it to the assets folder in ur project (make an assets folder)..
+
+![Screenshot of my game]('/assets/memory-game.png')
+.. u write ur file link in here
+
+## Getting Started
+
+### Play the Game
+.. u will put a link to the deployed game
+[Deployed Game] (link-goes-here-later)
+
+### How to play
+1. Press the start button
+2. Click a card
+3. Click a second card
+4. Memorize the matching cards
+
+### Installation
+No installation required! Simply clone the repo to your machine and open the `index.html` file in your browser.
+
+```
+get clone
+cd memory
+open index.html
+```
+
+### Technologies Used
+.. a dash is gonna give you a bullet point **text** will give u bold style
+- HTML
+- CSS
+- JavaScript
+
+###
--- js/app.js
+++ js/app.js
@@ -1,35 +1,25 @@
 /*-------------------------------- Constants --------------------------------*/
 const choices = ['❤️', '🤍', '🧡', '💛', '💚', '💙', '💜', '🤎', '💕', '💖'];
-const buttons = document.querySelectorAll('button')
+const buttons = document.querySelectorAll('.card')
+const startBtn = document.querySelector('#startbtn')
 /*-------------------------------- Variables --------------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
 
 /*-------------------------------- Functions --------------------------------*/
-// need a function play() here
-
-let time = 60
-const runTimer = setInterval(function() {
-    time -= 1
-    console.log('time left: ', time)
-}, 1000)
-
-setTimeout(function() {
-    clearInterval(runTimer)
-    console.log('Interval cleared. Function will no longer excute.')
-}, 30000)
-/*----------------------------- Event Listeners -----------------------------*/
-// when i click the button it should change the text content from the array above
-
-// buttons.forEach(function(button) {
-//     button.addEventListener('click', function(btn){
-//         button.style.backgroundColor ='yellow'
-//     });
-// });
-
+startBtn.textContent='Start'
 
 buttons.forEach(function(button) {
-    buttons.addEventListener('click', function() {
-        button.style.color = 'initial';
+    button.addEventListener('click', function() {
+        if (button.style.color === 'initial') {
+            button.style.color = 'transparent'
+        } else {
+            button.style.color = 'initial'
+        }
     });
 });
+
+
+// event
+
+function checkformatch (){}
